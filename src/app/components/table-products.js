7494b4b685e9dd5591396ba5b23881514a7df224
chuy12 <chapo7504@outlook.com>
2023-09-27 20:57:16 -0700

    tabla de productos

---
+++ src/app/components/table-products.js
@@ -0,0 +1,47 @@
+export default function TableProducts ({products = []}) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        lista
+                    </th> 
+                    <th>
+                        Productos.
+                    </th>
+                    <th>
+                        Precio.
+                    </th>
+                    <th>
+                        Cantidad.
+                    </th>
+                    <th>
+                        Precio/Uni.
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map((product,i) => (
+                    <tr key={product.key}>
+                        <td>
+                            {i + 1}
+                        </td>
+                        <td>
+                            {product?.name}
+                        </td>
+                        <td>
+                            {product?.price*product?.quantity} 
+                        </td>
+                        <td>
+                            {product?.quantity} 
+                        </td>
+                        <td>
+                            {product?.price} 
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,11 @@ import Counter from "./components/counter";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-3 h-full">
+    <main className=" flex h-full">
       <section className="border border-red-500">
         <Counter/>
       </section>
-      <section className="border border-red-500">
+      <section className="flex-1 border border-red-500">
         a
         <Counter/>
       </section>
