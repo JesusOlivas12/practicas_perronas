9ec93a9938eeaf060c3353ed687e5225ace7163d
Shino <dg047359@gmail.com>
2023-09-28 20:35:48 -0700

    Style

--- src/app/components/quick_access.js
+++ src/app/components/quick_access.js
@@ -4,7 +4,7 @@ export default function QuickAccess ({ quickAccessProducts = [] }) {
   return (
     <div className='p-2'>
       <h1 className='text-center'>Acceso rapido</h1>
-      <div className='grid grid-cols-2 gap-3 '>
+      <div className='grid grid-cols-2 gap-4 '>
         {quickAccessProducts.map(product => <ProductAccessEntity key={product?.key} product={product} />)}
       </div>
     </div>
@@ -31,7 +31,7 @@ function ProductAccessEntity ({ product }) {
   return (
     <button
       onClick={handleAddProduct}
-      className='w-20 h-20 bg-red-400 rounded-md'
+      className='w-20 h-20 bg-green-400 shadow-lg shadow-cyan-500/50 rounded-3xl ring-2 ring-cyan-500 ring-offset-3 ring-offset-slate-50 cyan:ring-offset-slate-900 text-white'
     >
       {product?.name}
     </button>
