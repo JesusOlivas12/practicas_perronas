871e8ac550ab206c44d3e38310fc5a4ff94dadcd
chuy12 <chapo7504@outlook.com>
2023-10-01 22:14:43 -0700

    ButtonBuy2

--- src/app/components/table-products/confirm-buy.js
+++ src/app/components/table-products/confirm-buy.js
@@ -1,9 +1,18 @@
 export default function Buy () {
   return (
     <div className='h-[15%]'>
-      <span>
-        total
+      <span className=''>
+        Total
       </span>
+      <div className='flex gap-10'>
+        <button className='bg-[#8CBA80] hover:bg-[#c1bdb366] text-black font-bold py-9 px-4 rounded-md shadow-lg btn'>
+          Domicilio
+        </button>
+        <button className='bg-[#8CBA80] hover:bg-[#c1bdb366] text-black font-bold py-9 px-4 rounded-md shadow-lg btn'>
+          Comprar
+        </button>
+
+      </div>
     </div>
 
   )
